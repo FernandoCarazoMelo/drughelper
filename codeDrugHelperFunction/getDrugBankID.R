d5066c4ed7c681ee21b171a6cfbe5ff3e2de51e4
Gimeno Combarro <mgimenoc@tecnun.es>
2021-02-03 12:07:44 +0100

    MG: Order Directory

---
+++ codeDrugHelperFunction/getDrugBankID.R
@@ -0,0 +1,31 @@
+#########################################################################################
+# FUNCTION GET CHEMBLID
+# Code Author: Marian Gimeno -- mgimenoc@tecnun.es
+# Conceptual Design: Fernando Carazo -- fcarazo@tecnun.es
+# Data Developer: Carlos Castilla -- ccastilla.1@tecnun.es
+#########################################################################################
+
+getDrugBankID<-function(DrugName){
+  DrugName<-toupper(DrugName)
+  load("./singleDrugSynonymsTabulatesDH_IDcreated.RData")
+  iix<-agrep(DrugName,singleDrugSynonyms$Drug_synonyms, max=3, ignore.case = T)
+  DB_ID<- singleDrugSynonyms$DB[iix]
+  if(length(DB_ID>0)){
+    return(unique(DB_ID))
+  }
+  if(length(DB_ID)==0){
+    stop("No DrugBank ID found")
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
