1f0f577502eeef2d6dea6fe1abe8b6015eed77be
Javier Garcia <a905125@alumni.unav.es>
2021-02-24 11:36:55 +0100

    mejorar código para la ultima columna de la tabla

--- codeJG/main.R
+++ codeJG/main.R
@@ -4,23 +4,22 @@
 # Conceptual Design: Fernando Carazo -- fcarazo@tecnun.es
 # Data Developer: Carlos Castilla -- ccastilla.1@tecnun.es
 #########################################################################################
-load("./data/input/2020-12-17version/singleDrugSynonymsTabulatesDH_IDcreated.RData")
+load("./data/input/2020-12-17version/singleDrugSynonymsTableUpdated.RData")
 source("./codeJG/DrugHelper.R")
 source("./codeJG/getCHEMBLID.R")
 source("./codeJG/getDrugBankID.R")
+source("./codeJG/updateSingleDrugSynonymsTable.R")
 
-
-DrugName<-"(4E)-4-AMI   NOHEX-4-ENOIC ACID"
+DrugName<-"cyclosporin"
 #DrugName<-"DB07604"
-
+DrugName<- toupper(DrugName)
 #Quitar espacio y signos de puntuación
 #sub solo quita el primer espacio del string, gsub quita todos a la vez
 DrugName <- gsub("[^[:alnum:]]", " ", DrugName)
 DrugName <- gsub("[[:blank:]]", "", DrugName)
 
-synonyms <- singleDrugSynonyms$Drug_synonyms(match(DrugName, singleDrugSynonyms$Drug_Synonyms_format))
+synonyms <- singleDrugSynonyms$Drug_synonyms[grep(DrugName, singleDrugSynonyms$Drug_Synonyms_format)]
 
-DH <- getDrugHelperID(DrugName)
 Chem<-getCHEMBLID(DrugName)
 
 DB<-getDrugBankID(Chem[1])
