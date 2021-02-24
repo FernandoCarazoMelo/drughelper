097b624eba5986e85d548c7bb6eba5f931542a9a
Javier Garcia <a905125@alumni.unav.es>
2021-02-24 18:14:06 +0100

    update Table

--- codeJG/updateSingleDrugSynonymsTable.R
+++ codeJG/updateSingleDrugSynonymsTable.R
@@ -1,10 +1,12 @@
-
-load("./data/input/2020-12-17version/singleDrugSynonymsTabulatesDH_IDcreated.Rdata")
-
-
-for (i in 1:nrow(singleDrugSynonyms)){
-  aux <- gsub("[[:blank:]]", "", singleDrugSynonyms$Drug_synonyms[i])
-  singleDrugSynonyms$Drug_Synonyms_format[i] <- gsub("[^[:alnum:];]", "", aux)
+updateTable <- function(singleDrugSynonyms){
+  load("./data/input/2020-12-17version/singleDrugSynonymsTabulatesDH_IDcreated.Rdata")
+  
+  
+  for (i in 1:nrow(singleDrugSynonyms)){
+    aux <- gsub("[[:blank:]]", "", singleDrugSynonyms$Drug_synonyms[i])
+    singleDrugSynonyms$Drug_Synonyms_format[i] <- gsub("[^[:alnum:];]", "", aux)
+  }
+  
+  View(singleDrugSynonyms)
+  save(singleDrugSynonyms, file = "./data/input/2020-12-17version/singleDrugSynonymsTableUpdated.RData")
 }
-
-View(singleDrugSynonyms)  
