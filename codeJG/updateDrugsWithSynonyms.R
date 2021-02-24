7fa4ee2b38abed619e717b474acaebac8557c873
Javier Garcia <a905125@alumni.unav.es>
2021-02-24 18:14:20 +0100

    .

--- codeJG/updateDrugsWithSynonyms.R
+++ codeJG/updateDrugsWithSynonyms.R
@@ -34,7 +34,7 @@ if (fAlreadyComputed == 0){
   
   # Get all unique drugs from 'Gene_Info' table
   
-  allDrugsDS <- NULL #variable de momento vacia 
+  allDrugsDS <- NULL #variable de momento vacía 
   for (i in 1:length(drugsDS)){
     print(i)
     currDrugs <- drugsDS[i]
