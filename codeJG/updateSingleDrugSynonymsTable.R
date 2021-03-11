c20def17d410c04693b47c0db27ad39d8a5d4da3
Javier Garcia <a905125@alumni.unav.es>
2021-03-11 19:41:18 +0100

    JG. some changes

--- codeJG/updateSingleDrugSynonymsTable.R
+++ codeJG/updateSingleDrugSynonymsTable.R
@@ -1,10 +1,10 @@
 updateTable <- function(singleDrugSynonyms){
-  load("./data/input/2020-12-17version/singleDrugSynonymsTabulatesDH_IDcreated.Rdata")
+  load("./data/input/2020-12-17version/singleDrugSynonymsChembl")
   
   
-  for (i in 1:nrow(singleDrugSynonyms)){
-    aux <- gsub("[[:blank:]]", "", singleDrugSynonyms$Drug_synonyms[i])
-    singleDrugSynonyms$Drug_Synonyms_format[i] <- gsub("[^[:alnum:];]", "", aux)
+  for (i in 1:nrow(singleDrugSynonymsChembl)){
+    aux <- gsub("[[:blank:]]", "", singleDrugSynonymsChembl$synonyms[i])
+    singleDrugSynonymsChembl$Drug_Synonyms_format[i] <- gsub("[^[:alnum:];]", "", aux)
   }
   
   singleDrugSynonyms <- as.data.table(singleDrugSynonyms)
