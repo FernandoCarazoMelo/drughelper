2796b51bf0bb3ba5fc92f875d8d06f5380d6a71b
Javier Garcia <a905125@alumni.unav.es>
2021-03-24 13:17:38 +0100

    JG start tempDF table

--- codeJG/checkDrugSynonym.R
+++ codeJG/checkDrugSynonym.R
@@ -22,23 +22,35 @@ checkDrugSynonym <- function(drugVector) {
                         
   for (i in 1:length(drugVector)) {
     
-    daf[i, 1] <- drugVector[i]
+          daf[i, 1] <- drugVector[i]
+          
+          DrugName <- formattingDrugName(drugVector[i])
+          
+          drugVector[i] <- DrugName #estas dos ultimas lineas tengo que mejorarlas,
+          #si encuentra mas de una coincidencia, devuelve la primera, debería
+          #devolver la que mas se parezca, hacer prueba con "morphine"
+          
+          logicVector <- agrepl(drugVector[i], datosChembl$synonyms_formatted, max.distance = 1)
+          
+          if (TRUE %in% logicVector){
+            daf[i, 2] <- TRUE
+          } else {
+            daf[i, 2] <- FALSE
+          }
+          
     
-    DrugName <- formattingDrugName(drugVector[i])
+    auxVectorsynonyms <- datosChembl$Drug[agrep(drugVector[1], datosChembl$synonyms_formatted, max.distance = 1)]
+    auxTable <-datosChembl[agrep(drugVector[1], datosChembl$synonyms_formatted, max.distance = 1), ]
     
-    drugVector[i] <- DrugName #estas dos ultimas lineas tengo que mejorarlas,
-    #si encuentra mas de una coincidencia, devuelve la primera, debería
-    #devolver la que mas se parezca, hacer prueba con "morphine"
-    
-    logicVector <- agrepl(drugVector[i], datosChembl$synonyms_formatted, max.distance = 1)
-    
-    if (TRUE %in% logicVector){
-      daf[i, 2] <- TRUE
-    } else {
-      daf[i, 2] <- FALSE
+    tempDF = data.frame(Index = integer(), Synonyms = character())
+    for (j in 1:length(auxVectorsynonyms)){
+      
+      list = unique(strsplit(auxTable$synonyms_formatted[j], split=";;;"))
+      tempDF = rbind(tempDF, data.frame(Index = rownames(auxTable)[j], Synonyms = unlist(list)))
     }
-    daf[i, 3] <- datosChembl$DrugHelper[agrep(drugVector[i], datosChembl$synonyms_formatted, max.distance = 1)[1]]
-    daf[i, 4] <- datosChembl$Drug[agrep(drugVector[i], datosChembl$synonyms_formatted, max.distance = 1)[1]]
+    
+    daf[i, 3] <- datosChembl$DrugHelper[rownames(datosChembl) == tempDF$Index[grep(drugVector[i], tempDF$Synonyms)]]
+    daf[i, 4] <- tempDF$Synonyms[grep(drugVector[i], tempDF[,2])]
     
   }
   return(daf)
