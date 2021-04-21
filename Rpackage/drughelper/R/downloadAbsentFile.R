760820274abe0e87428ef97d67698dddec0345a4
Javier Garcia <a905125@alumni.unav.es>
2021-04-21 11:40:16 +0200

    JG roxygen documentation

--- Rpackage/drughelper/R/downloadAbsentFile.R
+++ Rpackage/drughelper/R/downloadAbsentFile.R
@@ -1,7 +1,11 @@
+#' @title Download data from Chembl
+#' @description If it has not been downloaded yet, downloads data of drugs
+#' @export
+
 downloadAbsentFile <- function (){
-  
+
   if(!file.exists("./codeJG/datosChembl.tsv")) {
-    
+
     URL <- "https://raw.githubusercontent.com/jaaaviergarcia/drughelper/main/datosChembl.tsv"
     download.file(URL ,destfile = "./codeJG/datosChembl.tsv")
   }
