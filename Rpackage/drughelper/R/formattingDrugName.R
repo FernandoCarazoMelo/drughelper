625b2e41f60d08d9d0d5e30c98f9ebde97b4d0be
Javier Garcia <a905125@alumni.unav.es>
2021-04-21 11:40:32 +0200

    #roxygen documentation

--- Rpackage/drughelper/R/formattingDrugName.R
+++ Rpackage/drughelper/R/formattingDrugName.R
@@ -1,10 +1,14 @@
-# This function corrects and capitalizes the names of a given vector of drugs.
+#' @title Normalization of drug names
+#' @description  This function corrects and capitalizes the names of a given vector of drugs.
+#' @param A string with the name of the drug to be normalized
+#' @export
+#' @example formattingDrugNames("morphine")
 
  formattingDrugName <- function(DrugName) {
-  
+
   DrugName<- toupper(DrugName)
   DrugName <- gsub("[^[:alnum:]]", " ", DrugName)
   DrugName <- gsub("[[:blank:]]", "", DrugName)
-  
+
   return(DrugName)
 }
