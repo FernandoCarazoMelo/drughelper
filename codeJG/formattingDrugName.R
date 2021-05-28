251dff4b48d8055b402ddbb292e38e59d1fc1839
Javier Garcia <a905125@alumni.unav.es>
2021-05-28 11:49:17 +0200

    Changed documentation

--- codeJG/formattingDrugName.R
+++ codeJG/formattingDrugName.R
@@ -1,6 +1,6 @@
 #' @title Normalization of drug names
 #' @description  This function corrects and capitalizes the names of a given vector of drugs.
-#' @param A string with the name of the drug to be normalized
+#' @param DrugName string with the name of the drug to be normalized
 #' @export
 #' @example formattingDrugName("morphine")
 
