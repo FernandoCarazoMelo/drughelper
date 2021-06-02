72d5e9d40a84f999f7469910ea29d00a50d274e2
Javier Garcia <a905125@alumni.unav.es>
2021-06-02 18:56:50 +0200

    JG importfrom Documented

--- Rpackage/drughelper/R/downloadAbsentFile.R
+++ Rpackage/drughelper/R/downloadAbsentFile.R
@@ -1,6 +1,7 @@
 #' @title Download data from Chembl
 #' @description If it has not been downloaded yet, downloads data of drugs
 #' @param dir Name of the directory where data is downloaded
+#' @importFrom utils download.file
 #' @export
 
 downloadAbsentFile <- function (dir = tempdir()){
