78c9fb8f399539abcf3396aacee46628d2402442
Javier Garcia <a905125@alumni.unav.es>
2021-06-16 10:14:46 +0200

    Package documentation

---
+++ Rpackage/drughelper/R/drughelper-package.R
@@ -0,0 +1,34 @@
+#' @name{-package}
+#' @aliases{drughelper}
+#' @aliases{drughelper-package}
+#' @title{drughelper}
+#' @description{
+#'   To learn more about drughelper, start with the vignettes:
+#'     \code{browseVignettes(package = "drughelper")}
+#' }
+#'
+#'
+#'
+#' @author{
+#'   \strong{Maintainer}: Javier Garcia  \email{a905125@alumni.unav.es}
+#'
+#'   Authors:
+#'     \itemize{
+#'       \item Javier Garcia
+#'       \item Fernando Carazo
+#'     }
+#'
+#'   Other contributors:
+#'     \itemize{
+#'       \item Tecnun [copyright holder]
+#'     }
+#'
+#' }
+#'
+#'
+#'
+#'
+#' @keywords internal check
+"_PACKAGE"
+
+
