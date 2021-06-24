32e1af843848f33b8226d21ff4e7bd535eb479ad
Javier Garcia <a905125@alumni.unav.es>
2021-06-24 22:40:01 +0200

    JG space?

--- Rpackage/drughelper/inst/unitTests/test_checkDrugSynonym.R
+++ Rpackage/drughelper/inst/unitTests/test_checkDrugSynonym.R
@@ -3,4 +3,5 @@ test_checkDrugSynonym <- function() {
   obs <- tryCatch(checkDrugSynonym(drugVector = NULL), error=conditionMessage)
   checkIdentical("input drugVector must be a string vector", obs)
 
+
 }
